78972c27ed2c73f341c57f303aa1cbe5bbd84c9f
oda yuhei <oda@nexdoor.co.jp>
2023-03-14 16:28:43 +0900

    以前のverに戻した

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,15 +25,7 @@ const IndexPage: NextPage<Props> = ({initialImageUrl}) => {
   }
   return (
     <div className={styles.page}>
-      <button
-        onClick={handleClick}
-        style={{
-          backgroundColor: "#319795",
-          border: "none",
-          borderRadius: "4px",
-          color: "white",
-          padding: "4px 8px",
-        }}>
+      <button onClick={handleClick} className={styles.button}>
         他のにゃんこも見る
       </button>
       <div className={styles.frame}>
